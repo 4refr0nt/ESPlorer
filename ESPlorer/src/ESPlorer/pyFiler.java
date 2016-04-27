98abc8980181c2ad3a24e634b121d6001d171d7f
4refr0nt <4refr0nt@gmail.com>
2016-04-27 20:39:24 +0500

    Add rn2483 support

---
+++ ESPlorer/src/ESPlorer/pyFiler.java
@@ -0,0 +1,33 @@
+package ESPlorer;
+
+/**
+ *
+ * @author 4refr0nt
+ */
+public class pyFiler {
+    
+    public static final int version = 1;
+    private static String dir = "/";
+    
+    public pyFiler () {
+        
+    }
+    public String ListDir () {
+        return "";
+    }
+    public boolean put () {
+        return false;
+    }
+    public boolean get () {
+        return false;
+    }
+    public int size () {
+        return 0;
+    }
+    public String cd () {
+        return dir;
+    }
+    public String pwd () {
+        return dir;
+    }
+}
